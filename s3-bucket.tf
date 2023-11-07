d4d5e0f768023af1637d9299f5cebd6913b89e5d
saik8712 <139668888+saik8712@users.noreply.github.com>
2023-11-07 15:33:17 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,9 +3,9 @@ region = "us-east-1"
 access_key = "AKIA55O2WA5AQNYLGSA6"
 secret_key = "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"
 }
-resource "aws_s3_bucket" "saibucket" {
- bucket = "saik8712"
+resource "aws_s3_bucket" "s3saibucket" {
+ bucket = "saik87123"
  tags = {
- Name = "terraform"
+ Name = "terraform1"
  }
 }
